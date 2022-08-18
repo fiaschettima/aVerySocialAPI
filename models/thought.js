9473aa25d5c589d5586e55874ec5fd519fa7c82f
Matt F <fiaschettima@gmail.com>
2022-08-18 17:35:37 -0400

    user model finished

---
+++ models/thought.js
@@ -0,0 +1,20 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId:{
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody:{
+        type: String,
+        required: true,
+        max: 280,
+
+    },
+    usernname:{
+        type: String,
+        required: true,
+    },
+    createdAt:{}
+}
+)
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
     username: {
         type: String,
         required: true,
@@ -12,7 +12,29 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         validate: {
-            
+            function(email) {
+                return  /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email);
+            }
         },
-    }
-})
+        message: 'Please enter a valid email address!',
+    },
+    thoughts : [{
+        type: Schema.Types.ObjectId,
+        ref: 'thoughts'
+    },],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },],
+    toJSON : {
+        virtuals: true,
+    },
+    id: false,
+},
+);
+
+userSchema.virtuals('friendCount').get(function() {
+    return this.friends.length;
+});
+const User = model('User', userSchema)
+module.exports = User;
