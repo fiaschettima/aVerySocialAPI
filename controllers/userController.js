c6b269cfaf3fcc182ee551de104b319f4880792d
Matt F <fiaschettima@gmail.com>
2022-08-18 19:42:20 -0400

    delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, thought } = require('../models');
 
 module.exports = {
     getAllUser(req,res) {
@@ -16,9 +16,34 @@ module.exports = {
                     :res.json(user))
             .catch((err) => res.status(500).json(err));
     },
-    createNewUser(req,res){},
-    updateUser(req,res){},
-    deleteUser(req,res){},
+    createNewUser(req,res){
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err))
+    },
+    updateUser(req,res){
+        User.findByIdAndUpdate(
+            {_id: req.params.id},
+            {$set: req.body},
+            {runValidators: true, new: true}
+        ).then((user) =>
+            !user
+                ?  res.status(404).json({ message: 'No user with this id!' })
+                :  res.json(user)
+        ).catch((err) => {
+            res.status(500).json(err)
+        })
+    },
+    deleteUser(req,res){
+        User.findOneAndDelete({ _id : req.params.id })
+            .then((user) => 
+                !user
+                ? res.status(404).json({ message: "No User found with that ID" })
+                : thought.deleteMany( {_id: {$in: user.thoughts}})
+                ).catch((err) => {
+                    res.status(500).json(err)
+                })
+    },
     addFriend(req,res){},
     removeFriend(req,res){}
 }
