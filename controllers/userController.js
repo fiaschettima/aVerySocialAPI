f2a690b986a4d69e7b22776b4318af6090d747b0
Matt F <fiaschettima@gmail.com>
2022-08-18 19:34:05 -0400

    user find all and find one

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User } = require('../models/user');
+const { User } = require('../models');
 
 module.exports = {
     getAllUser(req,res) {
@@ -6,7 +6,15 @@ module.exports = {
         .catch((err) => res.status(500).json(err))
     },
     getOneUser(req,res) {
-
+        User.findOne({ _id: req.params.id})
+            .select('-__V')
+            .populate('thoughts')
+            .populate('friends')
+            .then((user) => 
+                !user
+                    ? res.status(400).json({message: 'No User found'})
+                    :res.json(user))
+            .catch((err) => res.status(500).json(err));
     },
     createNewUser(req,res){},
     updateUser(req,res){},
