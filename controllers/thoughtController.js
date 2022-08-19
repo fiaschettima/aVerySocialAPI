e0d50d7ea6f202da32848b9ecb20a2f6a95b0894
Matt F <fiaschettima@gmail.com>
2022-08-18 20:04:44 -0400

    thought controller add and update

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -18,8 +18,28 @@ module.exports = {
                 : res.json(thought)
             ).catch((err) => res.status(500).json(err))
     },
-    createNewThought(req,es){},
-    updateThought(req,res){},
+    createNewThought(req,es){
+        thought.create(req.body)
+            .then((thought) => {
+                return User.findOneAndUpdate(
+                    {_id: req.body.userId},
+                    {$addToSet : {thoughts : thought._id}},
+                    {new: true}
+                )
+            }).then((user) => 
+                !user   
+                ? res.status(404).json({ message: 'No user with that id' })
+                : res.json(user)    
+            ).catch((err) => res.status(500).json(err))
+    },
+    updateThought(req,res){
+        thought.findByIdAndUpdate({ _id: req.params.thoughtid})
+            .then((thought) =>
+                !thought
+                ? res.status(404).json({ message: 'No thought with that id' })
+                : res.json(thought)    
+            ).catch((err) => res.status(500).json(err))
+    },
     deleteThought(req,res){},
     addReaction(req,res){},
     removeReaction(req,res){}
